3f60cd0f43967c69fddacef40fad63ddae239c54
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-10-26 11:46:05 +0700

    Add bg image to base.html, change login page ui, create 404 html, include path to game app

--- game/views.py
+++ game/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'game/login.html')
+
+
+def page404(request, exception):
+    return render(request, 'game/404.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+
+handler404 = 'game.views.page404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
