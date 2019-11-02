e94679099f0f7b6690cdbf1745bf4c280fd7f2cc
Anant Arayanant <minecraftraguna@gmail.com>
2019-11-02 13:52:05 +0700

    add question_title/question_answer/hint to Question Model

--- game/forms.py
+++ game/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class QuestionForm(forms.Form):
+    title = forms.CharField(label='title', max_length=100000)
     question = forms.CharField(label='question', max_length=100000)
+    answer = forms.CharField(label='answer', max_length=100000)
+    hint = forms.CharField(label='hint', max_length=100000)
--- game/models.py
+++ game/models.py
@@ -2,4 +2,10 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
+    question_title = models.CharField(max_length=1000000)
     question_text = models.CharField(max_length=10000000)
+    question_answer = models.CharField(max_length=1000000)
+    answer_hint = models.CharField(max_length=1000000)
+
+    def __str__(self):
+        return self.question_title
