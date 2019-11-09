5b708f775c9e734a4592844b780e19f833018ad5
Anant Arayanant <minecraftraguna@gmail.com>
2019-11-09 13:40:37 +0700

    add Topic model/add topic and difficulty label to Question model

--- game/models.py
+++ game/models.py
@@ -5,12 +5,20 @@ from django.contrib.auth.models import User
 # from social_django import models as oauth_model_user
 
 # Create your models here.
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.topic_name
+
 
 class Question(models.Model):
     question_title = models.CharField(max_length=1000000)
     question_text = models.CharField(max_length=10000000)
     question_answer = models.CharField(max_length=1000000)
     answer_hint = models.CharField(max_length=1000000)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=10)
     pub_date = models.DateTimeField('date published', blank=True, null=True)
 
     def __str__(self):
