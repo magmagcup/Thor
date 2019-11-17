dcaef3a6d2abf96a0590974ff2450dd60ab729ab
Anant Arayanant <minecraftraguna@gmail.com>
2019-11-17 20:22:49 +0700

    answer box length depends on answer length

--- game/templatetags/create_box.py
+++ game/templatetags/create_box.py
@@ -2,19 +2,22 @@ from django import template
 
 register = template.Library()
 
-def string_check(value, first_char, last_char, marker, replacement):
+def string_check(value, first_char, last_char, marker, str_type):
     while (last_char in value):
         start = value.find(first_char, marker)
         end = value.find(last_char, start+1)
-        value = value[:start] + \
-            replacement + value[end+1:]
+        length = end - start
+        if str_type == 'answer':
+            replacement = f'<input type="text" size="{length}"></input>'
+        elif str_type == 'hint':
+            replacement = ''
+        value = value[:start] + replacement + value[end+1:]
         marker = end + 1
     return value
 
 @register.filter
 def create_box(value):
     last_box_mark, last_hint_mark = 0, 0
-    value = string_check(value, '_', '_', last_box_mark,
-                         '<input type="text"></input>')
-    value = string_check(value, '(', ')', last_box_mark, '')
+    value = string_check(value, '_', '_', last_box_mark, 'answer')
+    value = string_check(value, '(', ')', last_hint_mark, 'hint')
     return value
