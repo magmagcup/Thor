d40c8b5109d54155983bcabd69c1fe6fd7373787
Anant Arayanant <minecraftraguna@gmail.com>
2019-11-18 23:14:26 +0700

    add name attribute to input field

--- game/templatetags/create_box.py
+++ game/templatetags/create_box.py
@@ -2,22 +2,18 @@ from django import template
 
 register = template.Library()
 
-def string_check(value, first_char, last_char, marker, str_type):
-    while (last_char in value):
-        start = value.find(first_char, marker)
-        end = value.find(last_char, start+1)
-        length = end - start
-        if str_type == 'answer':
-            replacement = f'<input type="text" size="{length}"></input>'
-        elif str_type == 'hint':
-            replacement = ''
-        value = value[:start] + replacement + value[end+1:]
-        marker = end + 1
-    return value
-
 @register.filter
 def create_box(value):
-    last_box_mark, last_hint_mark = 0, 0
-    value = string_check(value, '_', '_', last_box_mark, 'answer')
-    value = string_check(value, '(', ')', last_hint_mark, 'hint')
+    last_box_mark = 0
+    answer_no = 1
+    while (']]' in value):
+        start = value.find('[[', last_box_mark)
+        mid = value.find('|', last_box_mark)
+        end = value.find(']]', last_box_mark)
+        ans_length = mid - start
+        replacement = f'<input type="text" size="{ans_length}" name="answer{answer_no}"></input>'
+        value = value[:start] + replacement + value[end+2:]
+        temp = last_box_mark
+        last_box_mark = end + 1
+        answer_no += 1
     return value
