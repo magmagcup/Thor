c0c61d5bfb9423003fd7952ad2e38631da3bd9c1
Champ2k <thehitmanza1@gmail.com>
2019-11-22 12:08:04 +0700

    set time

---
+++ game/static/js/time_interval.js
@@ -0,0 +1,29 @@
+var timeleft = 1;
+let is_running = false;
+let score = 0;
+document.getElementById("end").style.display = "none";
+document.getElementById("button1").style.display = "none";
+
+start = function () {
+    is_running = true;
+    if (is_running) {
+        let interval = setInterval(start = function() {
+        if (!is_running) {
+            clearInterval(interval);
+            document.getElementById("button1").style.display = "none";
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " second remaining";
+            timeleft++;
+        }
+        }, 1000)
+        document.getElementById("button1").style.display = "inline";
+        document.getElementById("start").style.display = "none";
+    }
+}
+
+stop = function() {
+    is_running = false;
+    timeleft = 1;
+    document.getElementById("countdown").innerHTML = "Game Over";
+    document.getElementById("end").style.display = "inline";
+}
