0e41dfc974ce3348bc915e0df82e1331a09812ed
Champ2k <thehitmanza1@gmail.com>
2019-11-27 16:09:41 +0700

    test model question/ test question form and answer form

---
+++ game/tests/test_answer_form.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.shortcuts import render, reverse
+from django.contrib.auth.models import User
+from game.forms import AnswerForm
+
+# Create your tests here.
+
+class AnswerFormTest(TestCase):
+    def setUp(self):
+        self.answer = "repeatedly"
+        self.answer_length = len(self.answer) 
+        self.hint = "Do again and again <adv>"
+        self.blank_form = AnswerForm(ans_length=self.answer_length,
+                                     box_length=str(self.answer_length)+"ch", hint=self.hint)
+
+    def test_html_answer_form_tag(self):
+        self.assertEquals(str(self.blank_form),'<tr><th></th><td><input type="text" name="answer" id="answer" style="height: 28px; font-size: 16px; " size="10ch" maxlength="10" title="Do again and again &lt;adv&gt;" onblur="checkAnswer()" required></td></tr>')
+
---
+++ game/tests/test_question_form.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.shortcuts import render, reverse
+from django.contrib.auth.models import User
+from game.forms import QuestionForm
+
+# Create your tests here.
+
+class QuestionFormTest(TestCase):
+    def setUp(self):
+        self.question = "Binary Search: Search a sorted array by [[repeatedly|Do again and again <adv>)]]"
+        self.title = "Binary Search"
+        self.difficulty = "easy"
+        self.topic = "Algorithm"
+        self.form = QuestionForm()
+        self.post_form = QuestionForm({
+                            'topic':self.topic, 
+                            'difficulty':self.difficulty, 
+                            'title':self.title, 
+                            'question':self.question
+                        })
+        self.html_tag_question_form = '<tr><th><label for="id_topic">Topic:</label></th><td><select name="topic" id="id_topic">\n  <option value="Naval History of WWII">Naval History of WWII</option>\n\n</select></td></tr>\n<tr><th><label for="id_difficulty">Difficulty:</label></th><td><select name="difficulty" id="id_difficulty">\n  <option value="easy">Easy</option>\n\n  <option value="normal">Normal</option>\n\n  <option value="hard">Hard</option>\n\n  <option value="extreme">Extreme</option>\n\n</select></td></tr>\n<tr><th><label for="title">Title</br>:</label></th><td><textarea name="title" cols="40" rows="10" id="title" style="height: 50px; font-size: 22px" class="mt-5" maxlength="100000" required>\n</textarea></td></tr>\n<tr><th><label for="question">Question</br>:</label></th><td><textarea name="question" cols="40" rows="10" id="question" style="height: 400px; font-size: 22px" class="mt-5" maxlength="100000" required>\n</textarea></td></tr>'
+
+    def test_form(self):
+        client = self.client
+        response = client.get(path='/form/')
+        status = response.status_code
+        self.assertEqual(status, 200)
+
+    def test_form_template(self):
+        client = self.client
+        response_form = client.get(path='/game/form/')
+        self.assertTemplateUsed(response_form, 'game/form.html')
+
+    def test_valid_form(self):
+        self.assertTrue(self.post_form.is_valid())
+
+    def test_invalid_form(self):
+        self.assertTrue(self.post_form.is_valid())
+        self.assertFalse(self.form.is_valid())
+    
+    def test_question_form_tag_html(self):
+        self.assertTrue(self.form)
+        self.assertTrue(self.post_form)
+        self.assertEquals(str(self.form),self.html_tag_question_form)
