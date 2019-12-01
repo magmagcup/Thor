45c1dddc9627b1ce65c870446fbe72a7f6aaed57
Champ2k <thehitmanza1@gmail.com>
2019-12-01 23:08:56 +0700

    test function discard form

--- game/tests/test_views.py
+++ game/tests/test_views.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 from game.models import Question, Topic, Answer, Best_score, Statistic
 from game.views import *
 
+def discard_form(question_id):
+    """Discard the Question Form."""
+    check = Question.objects.filter(pk=question_id)
+    if check:
+        question = Question.objects.get(pk=question_id)
+        question.delete()
+        return True
+    return False
+
 class ViewTest(TestCase):
     def setUp(self):
         self.client = Client()
@@ -12,6 +21,8 @@ class ViewTest(TestCase):
         self.user = User.objects.create_user(self.username,password=self.password)
         self.check = Statistic.objects.filter(user=self.user.id)
         self.status = True
+        self.topic = Topic.objects.create(topic_name="Topic")
+        self.question = Question.objects.create(topic=self.topic, question_title='Hello', question_text="world", difficulty="Easy")
     
     def test_get_stat(self):
         self.client.force_login(self.user)
@@ -29,4 +40,16 @@ class ViewTest(TestCase):
         self.assertEqual(stat.user.username, self.username)
         self.assertTrue(stat.user.password)
         self.assertTemplateUsed(response, 'game/base.html')
+    
+    def test_discard_form(self):
+        get_question = Question.objects.get(pk=self.question.id)
+        self.client.force_login(self.user)
+        response = self.client.get(path=f'{get_question.id}', follow=True)
+        status = response.status_code
+        self.assertEquals(status, 200)
 
+        self.assertTrue(get_question)
+        delete = discard_form(get_question.id)
+        self.assertTrue(delete)
+        none_question = discard_form(get_question.id)
+        self.assertFalse(none_question)
