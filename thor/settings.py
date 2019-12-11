1affa640c9c9adabab4631b4b1f8bd5271cafe2a
Champ2k <thehitmanza1@gmail.com>
2019-12-11 21:59:48 +0700

    edit structure

--- thor/settings.py
+++ thor/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'thor.urls'
 
 TEMPLATES = [
     {
@@ -92,7 +92,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'thor.wsgi.application'
 
 
 # Database
