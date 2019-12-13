f3d2b1c65a305e703a83f84b7ed4775939ae16da
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-12-13 12:41:38 +0700

    fix heroku

--- thor/settings.py
+++ thor/settings.py
@@ -180,7 +180,4 @@ else:
         BASE_DIR + '/static/',
     ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 django_heroku.settings(locals())
