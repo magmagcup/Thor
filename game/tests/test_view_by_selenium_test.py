3ffec1c0b72221e88e5c0d2a3a8ab852567450d1
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-12-15 23:23:08 +0700

    Revert necessary option

--- game/tests/test_view_by_selenium_test.py
+++ game/tests/test_view_by_selenium_test.py
@@ -11,6 +11,9 @@ class SeleniumTestCase(LiveServerTestCase):
 
     def setUp(self):
         options = webdriver.ChromeOptions()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         self.browser = webdriver.Chrome(chrome_options=options)
         # self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 10)
