e41f386001fc4c9e85a0f82a98c9c405f74c1a78
Jitta Koopratoomsiri <jitta.k@ku.th>
2019-12-15 20:30:14 +0700

    add test for homepage and howtoplay page

--- game/tests/test_view_by_selenium_test.py
+++ game/tests/test_view_by_selenium_test.py
@@ -7,26 +7,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-wait = WebDriverWait(driver, 10)
+# wait = WebDriverWait(driver, 10)
 
 
-# Click GMail login
-driver.find_element_by_xpath("//span[.='Google']").click()
+# # Click GMail login
+# driver.find_element_by_xpath("//span[.='Google']").click()
 
-# type email
-wait.until(EC.presence_of_element_located((By.ID, "Email"))).send_keys('...')
+# # type email
+# wait.until(EC.presence_of_element_located((By.ID, "Email"))).send_keys('...')
 
-# click next
-wait.until(EC.presence_of_element_located((By.ID, "next"))).click()
+# # click next
+# wait.until(EC.presence_of_element_located((By.ID, "next"))).click()
 
-# type password
-wait.until(EC.presence_of_element_located((By.ID, "Passwd"))).send_keys('...')
+# # type password
+# wait.until(EC.presence_of_element_located((By.ID, "Passwd"))).send_keys('...')
 
-# click signin
-wait.until(EC.presence_of_element_located((By.ID, "signIn"))).click()
+# # click signin
+# wait.until(EC.presence_of_element_located((By.ID, "signIn"))).click()
 
-# wait for the end of the redirection
-wait.until(EC.presence_of_element_located((By.ID, "nav-questions")))
+# # wait for the end of the redirection
+# wait.until(EC.presence_of_element_located((By.ID, "nav-questions")))
 
 class SeleniumTestCase(LiveServerTestCase):
     
@@ -39,3 +39,39 @@ class SeleniumTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.close()
         super(SeleniumTestCase, self).tearDown()
+
+    def test_find_h1_homepage(self):
+        """Test to find the h1 tag in the unauthorized homepage"""
+        self.browser.get(self.live_server_url + '/game/')
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual('THOR', header.text)
+
+    def test_find_h2_homepage(self):
+        """Test to find the h2 tag in the unauthorized homepage"""
+        self.browser.get(self.live_server_url + '/game/')
+        header = self.browser.find_element_by_tag_name('h2')
+        self.assertEqual('The God of Thunder', header.text)
+
+    def test_find_img_homepage(self):
+        """Test to find the img id in the unauthorized homepage"""
+        self.browser.get(self.live_server_url + '/game/')
+        image = self.browser.find_element_by_id('iconhome')
+        print(image.get_attribute('src'))
+
+    def test_find_img_howtopage(self):
+        """Test to find the img id in the instruction page"""
+        self.browser.get(self.live_server_url + '/game/howtoplay/')
+        image = self.browser.find_element_by_id('iconhowto')
+        print(image.get_attribute('src'))
+
+    def test_find_h1_howtopage(self):
+        """Test to find the h1 tag in the unauthorized homepage"""
+        self.browser.get(self.live_server_url + '/game/howtoplay/')
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual('How to play', header.text)
+
+    def test_find_example_howtopage(self):
+        """Test to find the example id in the instruction page"""
+        self.browser.get(self.live_server_url + '/game/howtoplay/')
+        image = self.browser.find_element_by_id('example')
+        print(image.get_attribute('src'))
