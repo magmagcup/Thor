133a45c7151ae2275730eb354db519a7bf4219f6
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-12-15 23:08:09 +0700

    Comment out  set up user"

--- game/tests/test_view_by_selenium_test.py
+++ game/tests/test_view_by_selenium_test.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.by import By
 
 class SeleniumTestCase(LiveServerTestCase):
 
@@ -59,20 +59,20 @@ class SeleniumTestCase(LiveServerTestCase):
         image = self.browser.find_element_by_id('example')
         print(image.get_attribute('src'))
     
-    def set_up_user(self):
-        """Setup user to login"""
-        self.browser.get('https://thor-thunder.herokuapp.com/game/')
-        self.browser.find_element_by_class_name('btn').click()
-        time.sleep(10)
-        mail = self.browser.find_element_by_xpath('//*[@id="identifierId"]')
-        mail.send_keys("gamethorthunder@gmail.com")
-        self.browser.find_element_by_id('identifierNext').click()
-        time.sleep(10)
-        password = self.browser.find_element_by_xpath('//*[@name="password"]')
-        password.send_keys("thorthunder1234")
-        self.browser.find_element_by_id('passwordNext').click()
-        time.sleep(10)
-
-    
-    def test_login_oauth(self):
-        self.set_up_user()
+    # def set_up_user(self):
+    #     """Setup user to login"""
+    #     self.browser.get('https://thor-thunder.herokuapp.com/game/')
+    #     self.browser.find_element_by_class_name('btn').click()
+    #     time.sleep(10)
+    #     mail = self.browser.find_element_by_xpath('//*[@id="identifierId"]')
+    #     mail.send_keys("gamethorthunder@gmail.com")
+    #     self.browser.find_element_by_id('identifierNext').click()
+    #     time.sleep(10)
+    #     password = self.browser.find_element_by_xpath('//*[@name="password"]')
+    #     password.send_keys("thorthunder1234")
+    #     self.browser.find_element_by_id('passwordNext').click()
+    #     time.sleep(10)
+    #
+    #
+    # def test_login_oauth(self):
+    #     self.set_up_user()
