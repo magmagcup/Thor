ece526d80991cd027260f6462d3526dd458491f5
Champ2k <thehitmanza1@gmail.com>
2019-12-15 20:24:23 +0700

    test login google using selenium

--- game/tests/test_view_by_selenium_test.py
+++ game/tests/test_view_by_selenium_test.py
@@ -7,35 +7,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-wait = WebDriverWait(driver, 10)
-
-
-# Click GMail login
-driver.find_element_by_xpath("//span[.='Google']").click()
-
-# type email
-wait.until(EC.presence_of_element_located((By.ID, "Email"))).send_keys('...')
-
-# click next
-wait.until(EC.presence_of_element_located((By.ID, "next"))).click()
-
-# type password
-wait.until(EC.presence_of_element_located((By.ID, "Passwd"))).send_keys('...')
-
-# click signin
-wait.until(EC.presence_of_element_located((By.ID, "signIn"))).click()
-
-# wait for the end of the redirection
-wait.until(EC.presence_of_element_located((By.ID, "nav-questions")))
-
 class SeleniumTestCase(LiveServerTestCase):
     
     
     def setUp(self):
         options = webdriver.ChromeOptions()
         self.browser = webdriver.Chrome(chrome_options=options)
+        self.driver = webdriver.Chrome(executable_path="chromedriver")
+        self.wait = WebDriverWait(self.browser, 10)
         super(SeleniumTestCase, self).setUp()
     
     def tearDown(self):
         self.browser.close()
         super(SeleniumTestCase, self).tearDown()
+    
+    def set_up_user(self):
+        """Setup user to login"""
+        self.browser.get('https://thor-thunder.herokuapp.com/game/')
+        self.browser.find_element_by_class_name('btn').click()
+
+        email = self.browser.find_element_by_xpath(
+            '//*[@id="identifierId"]')
+        email.send_keys("thehitmanza1@gmail.com")
+        self.browser.find_element_by_id('identifierNext').click()
+        time.sleep(20)
+        password = self.browser.find_element_by_xpath(
+            '//*[@name="password"]')
+        password.send_keys("0807741301")
+        self.browser.find_element_by_id('passwordNext').click()
+        time.sleep(10)
+
+    
+    def test_login_oauth(self):
+        self.set_up_user()
