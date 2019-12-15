74b428019d84753ffb962e4024b4b4d73e28a9e0
Champ2k <thehitmanza1@gmail.com>
2019-12-15 19:20:46 +0700

    init selenium test

---
+++ game/tests/test_view_by_selenium_test.py
@@ -0,0 +1,41 @@
+import time
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+wait = WebDriverWait(driver, 10)
+
+
+# Click GMail login
+driver.find_element_by_xpath("//span[.='Google']").click()
+
+# type email
+wait.until(EC.presence_of_element_located((By.ID, "Email"))).send_keys('...')
+
+# click next
+wait.until(EC.presence_of_element_located((By.ID, "next"))).click()
+
+# type password
+wait.until(EC.presence_of_element_located((By.ID, "Passwd"))).send_keys('...')
+
+# click signin
+wait.until(EC.presence_of_element_located((By.ID, "signIn"))).click()
+
+# wait for the end of the redirection
+wait.until(EC.presence_of_element_located((By.ID, "nav-questions")))
+
+class SeleniumTestCase(LiveServerTestCase):
+    
+    
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        self.browser = webdriver.Chrome(chrome_options=options)
+        super(SeleniumTestCase, self).setUp()
+    
+    def tearDown(self):
+        self.browser.close()
+        super(SeleniumTestCase, self).tearDown()
